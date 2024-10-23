e125b8848eaddf0edb0bd979475854d322142399
megrose10 <157438545+megrose10@users.noreply.github.com>
2024-10-23 15:39:43 -0600

    Update code.js

--- code.js
+++ code.js
@@ -13,26 +13,23 @@
     }
 }
 */
-function fib(n, array) {
+function fib(n) {
     if(n==0) {
         return array;
     }
-    for(let i=0; i<n; i++){
-        if(n==0) {
-            array.push(0);
-        }
-        else if(n==1) {
-            array.push(1);
-        }
-        else if(n==2) {
-            array.push(1);
-        }
-        else if(n==3) {
-            array.push(2);
-        }
-        else {
-            array.push(fib(n-1) + fib(n-2) + n);
-        }
+    if(n==0) {
+        return 0;
     }
-     return array;
+    else if(n==1) {
+        return 1;
+    }
+    else if(n==2) {
+        return 1;
+    }
+    else if(n==3) {
+        return 2;
+    }
+    else {
+        return fib(n - 1) + fib(n - 2);
+    }
 }
