bf51e27fecb5d011605b7f4575ab2f8ec6cafd8a
megrose10 <157438545+megrose10@users.noreply.github.com>
2024-10-23 15:41:34 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,18 +1,3 @@
-/*function fib(n) {
-    if(n==1) {
-        return 1;
-    }
-    else if(n==2) {
-        return 1;
-    }
-    else if(n==3) {
-        return 2;
-    }
-    else {
-        return fib(n-1) + fib(n-2) + n;
-    }
-}
-*/
 function fib(n) {
     if(n==0) {
         return 0;
