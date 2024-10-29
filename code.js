badac53c5163dc20bbd8c9eea25ca99aaeb95365
megrose10 <157438545+megrose10@users.noreply.github.com>
2024-10-29 13:04:04 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,20 +1,19 @@
-let fibArray = [];
 function fib(n) {
-    if(n==0) {
-        fibArray.push(0);
+    const fibArray = [];
+    function recurFib(x) {
+        if(n==0) {
+            fibArray.push(0);
+            return 0;
+        }
+        else if(n==1) {
+            fibArray.push(1);
+            return 1;
+        }
+        else {
+            fibArray.push(fib(n - 1) + fib(n - 2));
+            return (fib(n - 1) + fib(n - 2));
+        }
     }
-    else if(n==1) {
-        fibArray.push(1);
-    }
-    else if(n==2) {
-        fibArray.push(1);
-    }
-    else if(n==3) {
-        fibArray.push(2);
-    }
-    else {
-        fibArray.push(fib(n - 1) + fib(n - 2));
-    }
-
+    recurFib(n)
     return fibArray;
 }
