6dd14e14ecff0c8c7f63cd20f40a03e4cb61819b
megrose10 <157438545+megrose10@users.noreply.github.com>
2024-10-29 13:07:39 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,17 +1,17 @@
 function fib(n) {
     const fibArray = [];
     function recurFib(x) {
-        if(n==0) {
+        if(x==0) {
             fibArray.push(0);
             return 0;
         }
-        else if(n==1) {
+        else if(x==1) {
             fibArray.push(1);
             return 1;
         }
         else {
-            fibArray.push(fib(n - 1) + fib(n - 2));
-            return (fib(n - 1) + fib(n - 2));
+            fibArray.push(fib(x - 1) + fib(x - 2));
+            return (fib(x - 1) + fib(x - 2));
         }
     }
     recurFib(n)
